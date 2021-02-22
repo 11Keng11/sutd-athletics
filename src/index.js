02570b25b1f406c7f964a97ab2f4e196a84c12ea
BBFISH <44829092+xmliszt@users.noreply.github.com>
2021-02-23 00:06:04 +0800

    feat: Upload Run Score

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import { unstable_createRoot } from 'react-dom';
-import Homepage from './containers/Homepage.js';
+import HomePage from './containers/Homepage.js';
+import ResultsPage from './containers/Resultspage.js';
+import SignupPage from './containers/Signuppage.js';
+import UploadsPage from './containers/Uploadpage.js';
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <Homepage/>
-    </div>
-  );
-}
 
-unstable_createRoot(document.getElementById('root')).render(<App />);
+var hist = createBrowserHistory();
+unstable_createRoot(document.getElementById('root')).render(
+<Router history={hist}>
+  <Switch>
+    <Route path="/results-page" component={ResultsPage} />
+    <Route path="/signup-page" component={SignupPage} />
+    <Route path="/upload-page" component={UploadsPage} />
+    <Route path="/" component={HomePage} />
+  </Switch>
+</Router>);
 
 
